e527498287de8d0417252b05de9cff3b0176184c
Emirhan Erkan <117353243+Emrhn1@users.noreply.github.com>
2025-05-18 22:12:43 +0200

    Add files via upload

---
+++ Odev03/Pet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev03
+{
+   public abstract class Pet
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public int weigth { get; set; }
+
+        protected Pet(string name,int age,int weigth)
+        {
+            this.name = name;
+            this.age = age;
+            this.weigth = weigth;
+        }
+        public string getName()
+        {
+            return name;
+        }
+    }
+}
