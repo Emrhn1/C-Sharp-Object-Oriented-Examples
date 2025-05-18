78c87be02fea462323baa02188e047e6726ab51b
Emirhan Erkan <117353243+Emrhn1@users.noreply.github.com>
2025-05-18 22:12:10 +0200

    Add files via upload

---
+++ Odev1/Analyst.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev1
+{
+   public class Analyst: Employee
+    {
+        public int customerCount { get; set; }
+        public Analyst(int id,string name,int age,int salary,int customerCount):base(id,name,age,salary)
+        {
+            this.customerCount = customerCount;
+        }
+        public override void IncreaseSalary(int salary)
+        {
+            salary += customerCount * 5;
+            base.IncreaseSalary(salary);
+            
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $", Customer Count: {customerCount}";
+        }
+    }
+}
---
+++ Odev1/Project Manager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev1
+{
+   public class Project_Manager : Employee
+    {
+        public int ProjectCount { get; set; }
+        public Project_Manager(int id,string name,int age,int salary,int projectCount):base(id,name,age,salary)
+        {
+            this.ProjectCount = projectCount;
+        }
+
+        public override void IncreaseSalary(int salary)
+        {
+            salary += ProjectCount * 10;
+            base.IncreaseSalary(salary);
+            
+           
+        }
+        override public string ToString()
+        {
+            return base.ToString() + $", Project Count: {ProjectCount}";
+        }
+    }
+   
+}
